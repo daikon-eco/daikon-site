5481eb9e700f151b41ad8091ee067c20381207b0
Alexandre Duverger <42521043+aduverger@users.noreply.github.com>
2025-03-09 18:30:32 +0100

    Update site URL to use custom domain daikon.eco

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://daikon-eco.github.io',
-  base: '/daikon-site',
+  site: 'https://daikon.eco',
   integrations: [
     tailwind(),
     mdx(),
