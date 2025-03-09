ac59cda65ae866e98fc494b871ac42b4566245f5
Alexandre Duverger <42521043+aduverger@users.noreply.github.com>
2025-03-09 18:28:40 +0100

    Configure GitHub Pages deployment with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourdomain.com', // Replace with your actual domain
+  site: 'https://daikon-eco.github.io',
+  base: '/daikon-site',
   integrations: [
     tailwind(),
     mdx(),
